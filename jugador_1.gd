f33300b37d0e845e4afe5f6960a2c9df48a29426
KainnZer <estebanoria22@gmail.com>
2026-02-16 21:54:51 -0300

    XDEDEDEDEDEDEEDE
    prueba

---
+++ jugador_1.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Animaciones.play("queguapada")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+@warning_ignore("unused_parameter")
+func _physics_process(delta):
+	var direccion = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	
+	velocity = direccion * 200
+	move_and_slide()
